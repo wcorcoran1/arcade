e98be2dcec142843cff6f7f156edc071a29912d6
Wesley Corcoran <wesleycorcoran@Wesleys-MacBook-Air.local>
2022-03-01 17:10:19 -0600

    third update

--- index.js
+++ index.js
@@ -17,18 +17,31 @@ const gameState = {
         let player2= 'o'
         player2++
     }
-
-//   function that gets player 1 winning 
-  function winning(grid){
-       let playerOne = 'Player 1 Won'
-    if((gameState.b1 = gameState.player1) && (gameState.b2 = gameState.player1) && (gameState.b3 = gameState.player1)) {
-        return playerOne
-    } 
-    if((gameState.b1 = gameState.player1) && (gameState.b5 = gameState.player1) && (gameState.b9 = gameState.player1)){
-        return playerOne
+    
+    const winningCombo = {
+        [
+        [w1, w2, w3],
+        [w4, w5, w6],
+        [w7, w8, w9],
+        [w1, w4, w7],
+        [w2, w5, w8],
+        [w3, w6, w9],
+        [w1, w5, w9],
+        [w3, w5, w7]]
     }
-    console.log(playerOne)
-   }
+
+    
+//   function that gets player 1 winning
+//   function winning(grid){
+//        let playerOne = 'Player 1 Won'
+//     if((gameState.b1 = gameState.player1) && (gameState.b2 = gameState.player1) && (gameState.b3 = gameState.player1)) {
+//         return playerOne
+//     } 
+//     if((gameState.b1 = gameState.player1) && (gameState.b5 = gameState.player1) && (gameState.b9 = gameState.player1)){
+//         return playerOne
+//     }
+//     console.log(playerOne)
+//    }
 
    
     // gameScore function 
